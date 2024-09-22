0ddad15a5c60d64db6f8c9d096564cc1a2f031bb
Carlos Hernandez <carloshernandezp21@gmail.com>
2024-09-22 17:21:35 +0200

    refactor(autoNavegate): Move the useEffect to a new custom hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 // ---- ---- ---- HOOKS ---- ---- ----
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 // ---- ---- ---- SERVICES ---- ---- ----
 // import { getMovie } from './services/getMovie'
@@ -12,30 +12,16 @@ import { Header } from './Header'
 import { MovieSection } from './MovieSection'
 import { GenreSection } from './GenreSection'
 import movies from '../movies.json'
-import { Route, Routes, useNavigate, useLocation } from 'react-router-dom'
+import { useAutoNavegate } from '../customHooks/useAutoNavegate'
+import { Routes, Route } from 'react-router-dom'
 
 export const App = () => {
   // const [movies, setMovies] = useState()
   const [moviesToDisplay, setMoviesToDisplay] = useState(movies)
   const [search, setSearch] = useState(false)
-  const navegate = useNavigate()
-  const location = useLocation()
 
-  useEffect(() => { // agregar el location para saber en que pagin ase encuentra el usuario y asi determinar si hay que redirigirlo al pagina principal o ya se encuentra en la misma
-    let timeoutId
-    if (timeoutId) { clearTimeout(timeoutId) }
-    if (location.pathname !== '/') {
-      navegate('/')
-    }
-    timeoutId = setTimeout(() => {
-      setMoviesToDisplay(
-        filterMovies({ search, movies })
-      )
-    }, 400)
-    return () => {
-      clearTimeout(timeoutId)
-    }
-  }, [search])
+  // Navegate automatically everytime the user search a movie
+  useAutoNavegate({ setMoviesToDisplay, filterMovies, search, movies })
 
   return (
     <div className='container'>
---
+++ src/customHooks/useAutoNavegate.jsx
@@ -0,0 +1,25 @@
+import { useNavigate, useLocation } from 'react-router-dom'
+import { useEffect, useRef } from 'react'
+
+export const useAutoNavegate = ({ setMoviesToDisplay, filterMovies, search, movies }) => {
+  const navegate = useNavigate()
+  const location = useLocation()
+  const timeoutRef = useRef(null)
+  return (
+    useEffect(() => {
+      // Cancel the previous timeout if any
+      if (timeoutRef.current) { clearTimeout(timeoutRef.current) }
+
+      // Change the path if the user is in another
+      if (location.pathname !== '/') { navegate('/') }
+
+      // Asign a new timeout
+      timeoutRef.current = setTimeout(() => {
+        setMoviesToDisplay(filterMovies({ search, movies }))
+      }, 400)
+
+      // Clean the timeout when unmounting or updating the effect
+      return () => { clearTimeout(timeoutRef.current) }
+    }, [search])
+  )
+}
